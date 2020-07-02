dd192242eb5eb15c4277c8d2dd93c694d6471356
Dan-zai <danielcowpertwait@gmail.com>
2020-07-02 13:14:25 +1000

    Created registrations for Devise manipulation

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  root => 'dashboard#home'
+  root to: 'dashboard#home'
   get '/welcome' => 'dashboard#welcome'
   get '/dashboard' => "dashboard#dash"
 
 
   resources :owners
   resources :individuals
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
